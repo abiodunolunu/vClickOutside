e1c40f13b405c8764d5dd600a511100230d89a1c
abiodunolunu <abiodunolunu@gmail.com>
2022-10-23 01:06:29 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { ref } from "@vue/reactivity";
+import { onMounted } from "@vue/runtime-core";
+import { vClickOutside } from "./directives/clickOutside";
+
+const documentClickedCount = ref(0);
+const outsideDivClickCount = ref(0);
+const handleOutSideClick = () => {
+  outsideDivClickCount.value++;
+};
+
+onMounted(() => {
+  document.addEventListener("click", () => documentClickedCount.value++);
+});
+</script>
+
+<template>
+  <section>
+    <div>Document Click Count: {{ documentClickedCount }}</div>
+    <div v-click-outside="() => handleOutSideClick()" class="test">
+      Outside Click Count: {{ outsideDivClickCount }}
+    </div>
+  </section>
+</template>
+
+<style scoped>
+section {
+  height: 100vh;
+  background-color: black;
+  color: white;
+  font-size: 2rem;
+  font-weight: 700;
+
+  display: grid;
+  place-content: center;
+  gap: 1rem;
+  text-align: center;
+  user-select: none;
+}
+
+.test {
+  background: #bada55;
+  color: black;
+  padding: 2rem 4rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  cursor: pointer;
+}
+</style>
---
+++ src/directives/clickOutside.ts
@@ -0,0 +1,53 @@
+import { Directive } from "vue";
+
+export const vClickOutside: Directive<HTMLElement, Function> = (() => {
+  const checkIfClickedInside = (
+    event: Event,
+    element: HTMLElement,
+    callBackFn: Function
+  ) => {
+    const isInside = event.composedPath().includes(element);
+
+    const elementHasPreviouslyBeenClicked = element.dataset.clicked === "true";
+
+    if (isInside) {
+      element.dataset.clicked = "true";
+    } else {
+      element.dataset.clicked = "false";
+      if (elementHasPreviouslyBeenClicked) {
+        callBackFn();
+      }
+    }
+  };
+
+  const allListeners: { element: HTMLElement; listener: (e: Event) => void }[] =
+    [];
+
+  return {
+    created(element) {
+      element.dataset.clicked = "false";
+    },
+
+    mounted(element, binding) {
+      const myListener = (e: Event) => {
+        checkIfClickedInside(e, element, binding.value);
+      };
+      allListeners.push({ element, listener: myListener });
+      window.addEventListener("click", myListener);
+    },
+
+    unmounted(element) {
+      const eventIndex = allListeners.findIndex(
+        (listener) => listener.element === element
+      );
+
+      if (eventIndex !== -1) {
+        const listener = allListeners[eventIndex].listener;
+        window.removeEventListener("click", listener);
+        allListeners.splice(eventIndex, 1);
+      }
+
+      console.log(allListeners);
+    },
+  };
+})();
